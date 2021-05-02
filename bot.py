f2067941dc82da45675534a1d3163773566282f5
老房东 <huangdong@gmail.com>
2021-05-02 10:01:08 -0400

    加一个小功能

--- bot.py
+++ bot.py
@@ -47,6 +47,8 @@ if __name__ == '__main__':
     # 在这里加入功能
     # from cmdproc import admincmd
     # commands += admincmd.add_dispatcher(dispatcher)
+    from cmdproc import echo
+    commands += echo.add_dispatcher(dispatcher)
 
     updater.bot.set_my_commands(commands)
 
