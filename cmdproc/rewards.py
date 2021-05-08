9c58f39c4253303810ccc53d961ef6dae8092874
lben1216 <lben1216@hotmail.com>
2021-05-07 19:47:33 -0700

    Update rewards.py
    Optimize timing spending on array selection

--- cmdproc/rewards.py
+++ cmdproc/rewards.py
@@ -11,13 +11,15 @@ bad_rewards_array = ["oh no..", "So sorry...", "bad luck...", "next time will be
 def reward_cmd(update: Update, _:CallbackContext):
     rewardNumber = random.randint(0,200) #get random number from 0 - 200
     print(rewardNumber)
-    goodRewardWordIndex = random.randint(0,len(good_rewards_array) - 1) #get random index from good words list
-    badRewardWordIndex = random.randint(0,len(bad_rewards_array) - 1) #get random index from bad words list
-    print(goodRewardWordIndex, badRewardWordIndex)
+
     if rewardNumber == 0 :
+        badRewardWordIndex = random.randint(0,len(bad_rewards_array) - 1) #get random index from bad words list
+        print(badRewardWordIndex)
         #get value of index from bad words list
         update.message.reply_text(bad_rewards_array[badRewardWordIndex] + "! You got " + str(rewardNumber) + " XP! ")
     else :
+        goodRewardWordIndex = random.randint(0,len(good_rewards_array) - 1) #get random index from good words list
+        print(goodRewardWordIndex)
          #get value of index from good words list
         update.message.reply_text(good_rewards_array[goodRewardWordIndex] + "! You got " + str(rewardNumber) + " XP! ")
 
