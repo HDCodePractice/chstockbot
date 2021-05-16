ae1546c80fecbc0c3e3a6bd958da4c244e94061b
lben1216 <lben1216@hotmail.com>
2021-05-16 11:34:52 -0400

    Update report.py
    optimization III

--- cmdproc/report.py
+++ cmdproc/report.py
@@ -2,56 +2,33 @@ from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 
-def respose_txt(reporter_name, reporter_id, reportee_name,reportee_id):
-    msg = "name: {reporterName} ID: {reporterId} 举报了 \nname: {reporteeName} ID: {reporteeId} \n说了: \n".format(reporterName = reporter_name, reporterId = reporter_id, reporteeName = reportee_name, reporteeId = reportee_id)
+def respose_txt(reporter, reportee):
+    msg = f"""
+    Name: {reporter.id} ID: {reporter.full_name}
+    Name: {reportee.id} ID: {reportee.full_name}
+    say:
+    """
     return msg
 
 def report_user(update: Update, _:CallbackContext):
     incoming_message = update.message
     #verify if this is direct chat or forwarded chat
     print(incoming_message)
-    if incoming_message.reply_to_message is not None :
+    if incoming_message.reply_to_message:
         #forward_msg = []
-        reporter = incoming_message.chat.get_member(incoming_message.from_user.id) #举报人信息
-        if reporter["user"]["last_name"] is not None and reporter["user"]["first_name"] is not None :
-            reporter_name = reporter["user"]["first_name"] + " " + reporter["user"]["last_name"] 
-        elif reporter["user"]["last_name"] is not None and reporter["user"]["first_name"] is None :
-            reporter_name = reporter["user"]["last_name"] 
-        elif reporter["user"]["last_name"] is None and reporter["user"]["first_name"] is not None :
-            reporter_name = reporter["user"]["first_name"]
-        else :
-            reporter_name = "unknown user"
+        reporter = incoming_message.from_user #举报人信息
+        reporter_name = reporter.full_name
         #获得转发信息
 
 
         #forward_msg = update.effective_message
         print("this message is quoted from another message")
         #grab user information based on chat type (group or private)
-        if incoming_message.chat.type == None :
-            print("this is private chat")
-            reportee = incoming_message.reply_to_message.chat.get_member(incoming_message.reply_to_message.chat.id) #被举报人信息
-            if reportee["user"]["last_name"] is not None and reportee["user"]["first_name"] is not None :
-                reportee_name = reportee["user"]["first_name"] + " " + reportee["user"]["last_name"] 
-            elif reportee["user"]["last_name"] is not None and reportee["user"]["first_name"] is None :
-                reportee_name = reportee["user"]["last_name"] 
-            elif reportee["user"]["last_name"] is None and reportee["user"]["first_name"] is not None :
-                reportee_name = reportee["user"]["first_name"]
-            else :
-                reportee_name = "unknown user"
-            
-        else :
-            print("this is group chat")
-            reportee = incoming_message.reply_to_message.chat.get_member(incoming_message.reply_to_message.from_user.id) #被举报人信息
-            if reportee["user"]["last_name"] is not None and reportee["user"]["first_name"] is not None :
-                reportee_name = reportee["user"]["first_name"] + " " + reportee["user"]["last_name"] 
-            elif reportee["user"]["last_name"] is not None and reportee["user"]["first_name"] is None :
-                reportee_name = reportee["user"]["last_name"] 
-            elif reportee["user"]["last_name"] is None and reportee["user"]["first_name"] is not None :
-                reportee_name = reportee["user"]["first_name"]
-            else :
-                reportee_name = "unknown user"
+        print("this is private chat")
+        reportee = incoming_message.reply_to_message.from_user #举报人信息
+        reportee_name = reportee.full_name
         #send out message to chat
-        incoming_message.reply_text(respose_txt(reporter_name, reporter["user"]["id"], reportee_name, reportee["user"]["id"]))
+        incoming_message.reply_text(respose_txt(reporter,reportee))
         if incoming_message.reply_to_message.text is not None :
             forward_msg = incoming_message.reply_to_message.text
             incoming_message.reply_text(forward_msg)
