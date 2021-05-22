8438e6569d828c86e4d6a83d4cb5cc7f00976a82
lben1216 <lben1216@hotmail.com>
2021-05-22 01:01:18 -0400

    adding blocking/kicking function into bot

--- cmdproc/report.py
+++ cmdproc/report.py
@@ -1,16 +1,23 @@
+from datetime import datetime
+from os import error
 from telegram import Update, ForceReply
+import telegram
+from telegram.error import BadRequest
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
+admingroup = "-512402539"
+groups =["-512402539","-512402539"]
 
-def respose_txt(reporter, reportee):
+
+def respose_txt(reporter, reportee, forward_message):
     msg = f"""
-Name: {reporter.id} ID: {reporter.full_name}
-Name: {reportee.id} ID: {reportee.full_name}
-say:
+Name: {reporter.full_name} ID: {reporter.id}
+Name: {reportee.full_name} ID: {reportee.id}
+say: {forward_message}
     """
     return msg
 
-def report_user(update: Update, _:CallbackContext):
+def report_user(update: Update, callbackcontext:CallbackContext):
     incoming_message = update.message
     #verify if this is direct chat or forwarded chat
     print(incoming_message)
@@ -21,47 +28,103 @@ def report_user(update: Update, _:CallbackContext):
         #grab user information based on chat type (group or private)
         reportee = incoming_message.reply_to_message.from_user #举报人信息
         
+        #check if message is a text
+        if incoming_message.reply_to_message.text:
+            forward_message = incoming_message.reply_to_message.text
+        else :
+            forward_message = f"""not a message, but you can fetch it by searching message ID {incoming_message.reply_to_message.message_id}"""
         #send out message to chat
-        incoming_message.reply_text(respose_txt(reporter,reportee))
-        if incoming_message.reply_to_message.text is not None :
+        incoming_message.reply_text(respose_txt(reporter,reportee, forward_message))
+
+        #send direct message to admin group for audit
+        incoming_message.bot.send_message(admingroup,respose_txt(reporter,reportee, forward_message))
+
+
+
+    else :
+        incoming_message.reply_text("没有发现被举报人的信息，请重新选择包含被举报人的信息并回复/r")
+        print("this message doesnt contain reporter's name, please reply to the user's message you want to report")
+
+    #temperary disable echo previous message expect text
+'''         if incoming_message.reply_to_message.text != None :
             forward_msg = incoming_message.reply_to_message.text
             incoming_message.reply_text(forward_msg)
-        elif incoming_message.reply_to_message.animation is not None :
+        elif incoming_message.reply_to_message.animation != None :
             forward_msg = incoming_message.reply_to_message.animation.file_id
             incoming_message.reply_animation(forward_msg)
-        elif incoming_message.reply_to_message.audio is not None :
+        elif incoming_message.reply_to_message.audio != None :
             forward_msg = incoming_message.reply_to_message.audio.file_id
             incoming_message.reply_audio(forward_msg)
-        elif incoming_message.reply_to_message.sticker is not None :
+        elif incoming_message.reply_to_message.sticker != None :
             forward_msg =  incoming_message.reply_to_message.sticker.file_id
             incoming_message.reply_sticker(forward_msg)
-        elif incoming_message.reply_to_message.video is not None :
+        elif incoming_message.reply_to_message.video != None :
             forward_msg =  incoming_message.reply_to_message.video.file_id
             incoming_message.reply_video(forward_msg)
-        elif incoming_message.reply_to_message.voice is not None :
+        elif incoming_message.reply_to_message.voice != None :
             forward_msg =  incoming_message.reply_to_message.voice.file_id
             incoming_message.reply_voice(forward_msg)
-        elif incoming_message.reply_to_message.venue is not None :
+        elif incoming_message.reply_to_message.venue != None :
             forward_msg =  incoming_message.reply_to_message.venue.file_id
             incoming_message.reply_venue(forward_msg) 
-        elif incoming_message.reply_to_message.photo is not None :
+        elif incoming_message.reply_to_message.photo != None :
             forward_msg =  incoming_message.reply_to_message.photo.copy
             incoming_message.reply_photo(forward_msg)    
-        elif incoming_message.reply_to_message.poll is not None :
+        elif incoming_message.reply_to_message.poll != None :
             forward_msg =  incoming_message.reply_to_message.poll.id
             incoming_message.reply_poll(forward_msg)
-        elif incoming_message.reply_to_message.document is not None :
+        elif incoming_message.reply_to_message.document != None :
             forward_msg = incoming_message.reply_to_message.document.file_id
             incoming_message.reply_document(forward_msg)
-        elif incoming_message.reply_to_message.text_html is not None :
+        elif incoming_message.reply_to_message.text_html != None :
             forward_msg = incoming_message.reply_to_message.text_html
             incoming_message.reply_html(forward_msg)           
         else:
             forward_msg = incoming_message.reply_to_message.text
-            incoming_message.reply_text(forward_msg) 
-    else :
-        incoming_message.reply_text("没有发现被举报人的信息，请重新选择包含被举报人的信息并回复/r")
-        print("this message doesnt contain reporter's name, please reply to the user's message you want to report")
+            incoming_message.reply_text(forward_msg) '''
+          
+    
+
+def kick_member(update: Update, _:CallbackContext): #移除并拉黑举报人
+    forwarding_message = update.message
+    #check which command user sending
+    command = forwarding_message.text
+    #check if reply_to_message exist
+    if forwarding_message.reply_to_message.text and "say:" in forwarding_message.reply_to_message.text :
+        
+        if command == "/kk" :
+        #get reporter inforamtion
+            member_info = forwarding_message.reply_to_message.text.split("\n")[0]
+            member_id = member_info.split("ID: ")[-1]
+        elif command == "/kr" :
+            member_info = forwarding_message.reply_to_message.text.split("\n")[0]
+            member_id = member_info.split("ID: ")[-1]
+        else:
+            forwarding_message.reply_text("no command found, please re-try")
+        print(forwarding_message)
+        #make sure the name and id being sent to admin group
+
+        for group in groups:
+            #kick reporter
+            try:
+                forwarding_message.bot.kick_chat_member(group,member_id)
+                forwarding_message.reply_text(f"""已在群组{group}中删除用户：{member_id}""")
+            except :
+                forwarding_message.reply_text(f"""缺少管理权限，无法在群组{group}中删除用户：{member_id}; 请联系管理员删除""")
+            #block reporter
+            try:
+                forwarding_message.bot.restrict_chat_member(group,member_id, datetime.now)
+                forwarding_message.reply_text(f"""已在群组{group}中拉黑用户：{member_id}""")
+            except :
+                forwarding_message.reply_text(f"""缺少管理权限，无法在群组{group}中拉黑用户：{member_id}; 请联系管理员删除""")
+
+    else:
+        forwarding_message.reply_text(f"""没有发现被举报人的信息，请重新选择包含被举报人的信息并回复{command}""")    
+
+
+
 def add_dispatcher(dp):
     dp.add_handler(CommandHandler("r", report_user))
+    dp.add_handler(CommandHandler("kr", kick_member))
+    dp.add_handler(CommandHandler("kk", kick_member))
     return []
