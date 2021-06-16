658dd7e85a839fdd1e5543640ed37980bb375b99
stephenzhu01 <82696093+stephenzhu01@users.noreply.github.com>
2021-06-16 19:59:18 +0800

    0613的作业（SZ）

--- sendxyh.py
+++ sendxyh.py
@@ -5,31 +5,53 @@ from telegram import Bot
 from pandas_datareader._utils import RemoteDataError
 
 symbols = [["SPY",10,50],["QQQ",13,55,200],["RBLX",13,55,200]]
-notifychat = -1001430794202
+notifychat = -1001409640737
 adminchat = -1001250988031
-# symbols = [["SPY",10,50]]
+ds = ['stooq','yahoo']
+#symbols = [["SPY",10,50]]
+#issue 18, 22, 26 作业
 
 def help():
     return "'sendxyh.py -c configpaht'"
 
 def cal_symbols_avg_stooq(symbol:str,avgs:list):
-    pass
-
-def cal_symbols_avg_yahoo(symbol:str,avgs:list):
     start = datetime.date.today() - datetime.timedelta(days=365)
     end = datetime.date.today()
+    current_day = datetime.date.today()
+    df = web.DataReader(symbol.upper(), ds[0], start=start,end=end)
+    if current_day == df.index.date[0]:
+        try:
+            message = f"{symbol.upper()}价格: {df['Close'][0]:0.2f}({df['Low'][0]:0.2f} - {df['High'][0]:0.2f}) \n"
+            for avg in avgs:
+                if df.count()[0] > avg :
+                    if f"{df['Close'][0]:0.2f}" > f"{df.head(avg)['Close'].mean():0.2f}":
+                        message += f"🟢{avg} 周期均价：{df.head(avg)['Close'].mean():0.2f}\n"
+                    else:
+                        message += f"🔴{avg} 周期均价：{df.head(avg)['Close'].mean():0.2f}\n"
+                else:
+                    message += f"{avg} 周期均价因时长不足无法得出\n"
+            #print (f"{message}\n")
+        except RemoteDataError:
+            return f"{symbol.upper()}丢失了\n"
+            #print( f"{symbol.upper()}丢失了\n")
+    else:
+        message = f"现在是 {current_day}， 不是交易时间哦，休息一下吧。\n"
+        #print (message)
 
-    try:
-        df = web.DataReader(symbol.upper(), 'yahoo',start=start,end=end)
-        message = f"{symbol.upper()}价格: {df['Close'][-1]:0.2f}({df['Low'][-1]:0.2f} - {df['High'][-1]:0.2f}) \n"
-        for avg in avgs:
-            if df.count()[0] > avg :
-                message += f"{avg} 周期均价：{df.tail(avg)['Adj Close'].mean():0.2f}\n"
-            else:
-                message += f"{avg} 周期均价因时长不足无法得出\n"
-        return f"{message}\n"
-    except RemoteDataError:
-        return f"{symbol}丢失了\n"
+# def cal_symbols_avg_yahoo(symbol:str,avgs:list):
+#     start = datetime.date.today() - datetime.timedelta(days=365)
+#     end = datetime.date.today()
+#     try:
+#         df = web.DataReader(symbol.upper(), ds[1],start=start,end=end)
+#         message = f"{symbol.upper()}价格: {df['Close'][-1]:0.2f}({df['Low'][-1]:0.2f} - {df['High'][-1]:0.2f}) \n"
+#         for avg in avgs:
+#             if df.count()[0] > avg :
+#                 message += f"{avg} 周期均价：{df.tail(avg)['Adj Close'].mean():0.2f}\n"
+#             else:
+#                 message += f"{avg} 周期均价因时长不足无法得出\n"
+#         return f"{message}\n"
+#     except RemoteDataError:
+#         return f"{symbol}丢失了\n"
 
 
 if __name__ == '__main__':
@@ -59,9 +81,9 @@ if __name__ == '__main__':
     message = "🌈🌈🌈当日天相🌈🌈🌈: \n"
     try:
         for symbol in symbols:
-            message += cal_symbols_avg_yahoo(symbol[0],symbol[1:])
+            message += cal_symbols_avg_stooq(symbol[0],symbol[1:])
         bot.send_message(notifychat,message)
-        # bot.send_message(adminchat,f"向{notifychat}发送成功夕阳红:\n{message}")
+        bot.send_message(adminchat,f"向{notifychat}发送成功夕阳红:\n{message}")
     except Exception as err:
         print(err)
         bot.send_message(adminchat,f"今天完蛋了，什么都不知道，快去通知管理员，bot已经废物了出的问题是:\n{err}")
