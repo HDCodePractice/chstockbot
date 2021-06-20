8aede14fa0b8d265da50688e338f9cf447cd4bbe
Danny Huang <huangdong@gmail.com>
2021-06-20 11:34:05 -0400

    帮助修改一个noneType的问题

--- sendxyh.py
+++ sendxyh.py
@@ -24,7 +24,7 @@ def cal_symbols_avg_stooq(symbol:str,avgs:list):
             message = f"{symbol.upper()}价格: {df['Close'][0]:0.2f}({df['Low'][0]:0.2f} - {df['High'][0]:0.2f}) \n"
             for avg in avgs:
                 if df.count()[0] > avg :
-                    if f"{df['Close'][0]:0.2f}" > f"{df.head(avg)['Close'].mean():0.2f}":
+                    if df['Close'][0] > df.head(avg)['Close'].mean():
                         message += f"🟢{avg} 周期均价：{df.head(avg)['Close'].mean():0.2f}\n"
                     else:
                         message += f"🔴{avg} 周期均价：{df.head(avg)['Close'].mean():0.2f}\n"
@@ -37,21 +37,22 @@ def cal_symbols_avg_stooq(symbol:str,avgs:list):
     else:
         message = f"现在是 {current_day}， 不是交易时间哦，休息一下吧。\n"
         #print (message)
+        return ""
 
-# def cal_symbols_avg_yahoo(symbol:str,avgs:list):
-#     start = datetime.date.today() - datetime.timedelta(days=365)
-#     end = datetime.date.today()
-#     try:
-#         df = web.DataReader(symbol.upper(), ds[1],start=start,end=end)
-#         message = f"{symbol.upper()}价格: {df['Close'][-1]:0.2f}({df['Low'][-1]:0.2f} - {df['High'][-1]:0.2f}) \n"
-#         for avg in avgs:
-#             if df.count()[0] > avg :
-#                 message += f"{avg} 周期均价：{df.tail(avg)['Adj Close'].mean():0.2f}\n"
-#             else:
-#                 message += f"{avg} 周期均价因时长不足无法得出\n"
-#         return f"{message}\n"
-#     except RemoteDataError:
-#         return f"{symbol}丢失了\n"
+def cal_symbols_avg_yahoo(symbol:str,avgs:list):
+    start = datetime.date.today() - datetime.timedelta(days=365)
+    end = datetime.date.today()
+    try:
+        df = web.DataReader(symbol.upper(), ds[1],start=start,end=end)
+        message = f"{symbol.upper()}价格: {df['Close'][-1]:0.2f}({df['Low'][-1]:0.2f} - {df['High'][-1]:0.2f}) \n"
+        for avg in avgs:
+            if df.count()[0] > avg :
+                message += f"{avg} 周期均价：{df.tail(avg)['Adj Close'].mean():0.2f}\n"
+            else:
+                message += f"{avg} 周期均价因时长不足无法得出\n"
+        return f"{message}\n"
+    except RemoteDataError:
+        return f"{symbol}丢失了\n"
 
 
 if __name__ == '__main__':
@@ -85,5 +86,5 @@ if __name__ == '__main__':
         bot.send_message(notifychat,message)
         bot.send_message(adminchat,f"向{notifychat}发送成功夕阳红:\n{message}")
     except Exception as err:
-        print(err)
+        err.print_exc()
         bot.send_message(adminchat,f"今天完蛋了，什么都不知道，快去通知管理员，bot已经废物了出的问题是:\n{err}")
