61c522048e84e11b7b1eab55e0c42cd7388024b0
Danny Huang <huangdong@gmail.com>
2021-07-04 23:12:54 -0400

    顺手写一个自动加@botname的helper

--- musicbot/modules/song.py
+++ musicbot/modules/song.py
@@ -1,7 +1,8 @@
 from pyrogram import Client,filters
 from .. import config
+from ..helpers.filters import command
 
-@Client.on_message(filters.command(["s",f"s@{config.BOT_USERNAME}"]))
+@Client.on_message(command("s"))
 async def song(clent,message):
     print(message)
     await message.reply_text(message.text)
