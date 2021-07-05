a5399433500885228f5fdf0035920d2f66cd9aa4
Danny Huang <huangdong@gmail.com>
2021-07-05 09:18:23 -0400

    为了查询队列，加入一个getlist功能

--- musicbot/services/queues/__init__.py
+++ musicbot/services/queues/__init__.py
@@ -3,5 +3,6 @@ from .queues import get
 from .queues import is_empty
 from .queues import put
 from .queues import task_done
+from .queues import getlist
 
-__all__ = ['clear', 'get', 'is_empty', 'put', 'task_done']
+__all__ = ['clear', 'get', 'is_empty', 'put', 'task_done', 'getlist']
