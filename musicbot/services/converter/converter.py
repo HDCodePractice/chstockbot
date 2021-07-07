81f29a320e182e3d3555a271f0e130402b8f705d
Danny Huang <huangdong@gmail.com>
2021-07-07 18:02:13 -0400

    编码时不输出error log

--- musicbot/services/converter/converter.py
+++ musicbot/services/converter/converter.py
@@ -14,8 +14,7 @@ async def convert(file_path: str) -> str:
         format='s16le',
         acodec='pcm_s16le',
         ac=2,
-        ar='48k',
-        loglevel='error'
+        ar='48k'
     ).overwrite_output().run()
     return out
 
