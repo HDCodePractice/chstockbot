273336e47d744ec28531765c12ddd104b5a43e40
Danny Huang <huangdong@gmail.com>
2021-07-08 23:04:36 -0400

    加入音量控制

--- musicbot/modules/play.py
+++ musicbot/modules/play.py
@@ -7,6 +7,7 @@ from ..helpers.filters import command
 from ..helpers.chat_id import get_chat_id
 
 from ..services.callsmusic import callsmusic
+from ..services.queues import queues
 
 
 @Client.on_message(command("join") & ~filters.edited)
@@ -53,4 +54,36 @@ async def play(_,message:Message):
         await sleep(5)
         await mm.delete()
     await sleep(1)
+    await message.delete()
+
+@Client.on_message(command("volume") & ~filters.edited)
+async def volume(_,message:Message):
+    if len(message.command) < 2:
+        mm = await message.reply("请告诉我音量(0-200)值")
+    else:
+        chat_id = get_chat_id(message.chat)
+        await callsmusic.volume(chat_id,message.command[1])
+    await sleep(5)
+    if mm : 
+        await mm.delete()
+    await message.delete()
+
+@Client.on_message(command("skip") & ~filters.edited)
+async def skip(_,message:Message):
+    chat_id = get_chat_id(message.chat)
+    if chat_id not in callsmusic.active_chats:
+        mm = await message.reply_text("快去用 `/s 歌曲` 点个歌再考虑是不是要跳过好不好")
+        await sleep(5)
+        await mm.delete()
+    else:
+        await callsmusic.skip(chat_id)
+    await message.delete()
+
+
+@Client.on_message(command("s") & ~filters.edited)
+async def state(_,message:Message):
+    chat_id = get_chat_id(message.chat)
+    print("queue:\n",queues.getlist(chat_id) )
+    print("inputfile:\n",callsmusic.instances[chat_id].input_filename)
+    print("state",callsmusic.active_chats[chat_id])
     await message.delete()
