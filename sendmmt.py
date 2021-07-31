23af5e7ef9803882f82a168594ce373e9bf8fbd6
lben1216 <lben1216@hotmail.com>
2021-07-30 23:20:57 -0400

    Update sendmmt.py

--- sendmmt.py
+++ sendmmt.py
@@ -34,8 +34,8 @@ def cal_mmt_profit(symbol,ds,principle=100,start=datetime.date.today(),end=datet
                     second_wednesday_count +=1 #如果当天是当月第二个周三，大毛毛个数+1
                     dmm_stock_number += principle/price #获取大毛毛股数
                 xmm_stock_number += principle/price #获取小毛毛股数
-            xmm_profit = {"current_price": df["Close"][-1], "current_profit":xmm_stock_number * df["Close"][-1],"total_principle":principle * len(date_list),"profit_percentage": 1 - principle * len(date_list)/(xmm_stock_number * df["Close"][-1]) } 
-            dmm_profit = {"current_price": df["Close"][-1], "current_profit":dmm_stock_number * df["Close"][-1],"total_principle":principle * second_wednesday_count, "profit_percentage": 1 - principle * second_wednesday_count/(dmm_stock_number * df["Close"][-1])} 
+            xmm_profit = {"current_price": df["Close"][-1], "current_profit":xmm_stock_number * df["Close"][-1],"total_principle":principle * len(date_list),"profit_percentage": (xmm_stock_number * df["Close"][-1])/(principle * len(date_list)) - 1 } 
+            dmm_profit = {"current_price": df["Close"][-1], "current_profit":dmm_stock_number * df["Close"][-1],"total_principle":principle * second_wednesday_count, "profit_percentage": (dmm_stock_number * df["Close"][-1])/(principle * second_wednesday_count) - 1} 
             break #当数据源成功读取并处理数据后，从当前程序break并返回信息； 防止程序运行所有的数据源
         except NotImplementedError:
             err_msg += f"当前数据源{datasource}不可用"
