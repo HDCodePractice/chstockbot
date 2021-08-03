496f56b1a935ddc6864392f93d56c9454b259b10
Danny Huang <huangdong@gmail.com>
2021-08-03 13:50:14 -0400

    没有config.json不会退出了

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     except FileNotFoundError:
         print(f"config.json not found.Generate a new configuration file in {config.config_file}")
         config.set_default()
-        sys.exit(2)
+
     ENV = config.ENV
 
     updater = Updater(ENV.BOT_TOKEN, use_context=True)
