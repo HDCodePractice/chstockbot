4af3e778770021676001fa00480ab4092480937e
Danny Huang <huangdong@gmail.com>
2021-08-23 21:12:09 -0400

    加入class属性说明

--- stockutil/index.py
+++ stockutil/index.py
@@ -5,10 +5,15 @@ class IndexError(Exception):
     pass
 
 class Index:
+    #指数代码
     symbol = None
+    #均线周期
     ma=None
+    #指数成分股列表
     tickers = []
+    #本地数据存储路径   
     local_store = ""
+    # 下载指数成分股的数据源
     sources = {
         "NDX" : ["https://en.wikipedia.org/wiki/Nasdaq-100",3,"Ticker"],
         "SPX" : ["https://en.wikipedia.org/wiki/List_of_S%26P_500_companies",0,"Symbol"]
