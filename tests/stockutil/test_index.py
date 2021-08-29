5c6e8085e6773634f2d5283888df6205dd048c8f
Danny Huang <huangdong@gmail.com>
2021-08-29 12:17:43 -0400

    加入markets的测试用例

--- tests/stockutil/test_index.py
+++ tests/stockutil/test_index.py
@@ -24,3 +24,22 @@ def test_index_error_symbol():
         Index("BBLL")
     exec_msg = e.value.args[0]
     assert exec_msg == "BBLL 不在我们的支持列表中"
+
+def test_index_get_tickers_list_markets(shared_datadir):
+    index = Index("nyse",from_s="markets",local_store=f"{shared_datadir}")
+    index.get_tickers_list()
+    assert len(index.tickers) == 1
+    assert "OGN" in index.tickers
+    
+    index = Index("nasdaq",from_s="markets",local_store=f"{shared_datadir}")
+    index.get_tickers_list()
+    assert len(index.tickers) == 2
+    assert "AAPL" in index.tickers
+    assert "GOEV" in index.tickers
+
+def test_index_error_market_symbol(shared_datadir):
+    from stockutil.index import IndexError
+    with pytest.raises(IndexError) as e:
+        Index("aaaa",from_s="markets",local_store=f"{shared_datadir}")
+    exec_msg = e.value.args[0]
+    assert exec_msg == "aaaa 不在我们的支持列表中"
