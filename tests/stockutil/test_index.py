9b433284a1478689916656c58a2771de894dd55a
lben1216 <lben1216@hotmail.com>
2021-08-29 12:22:21 -0400

    加入测试代码

--- tests/stockutil/test_index.py
+++ tests/stockutil/test_index.py
@@ -24,3 +24,25 @@ def test_index_error_symbol():
         Index("BBLL")
     exec_msg = e.value.args[0]
     assert exec_msg == "BBLL 不在我们的支持列表中"
+
+
+def test_market_index_error_symbol(shared_datadir):
+    from stockutil.index import IndexError
+    with pytest.raises(IndexError) as e:
+        Index("BBLL","markets",local_store=f"{shared_datadir}")
+    exec_msg = e.value.args[0]
+    assert exec_msg == "BBLL 不在我们的支持列表中"
+
+def test_index_get_market_ticker_list(shared_datadir):
+    nasdaq = Index("nasdaq","markets",local_store=f"{shared_datadir}")
+    nasdaq.get_tickers_list()
+    # print(nasdaq.tickers)
+    assert len(nasdaq.tickers) == 2
+    assert "AAPL" in nasdaq.tickers
+
+    nyse = Index("nyse","markets",local_store=f"{shared_datadir}")
+    nyse.get_tickers_list()
+    # print(nyse.tickers)
+    assert len(nyse.tickers) == 1
+    assert "OGN" in nyse.tickers
+ 
