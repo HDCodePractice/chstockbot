072d58344078ff492ca178108f937cec0ecdadc4
Danny Huang <huangdong@gmail.com>
2021-08-28 21:51:49 -0400

    加入定投利润率unit test

--- tests/stockutil/test_ticker.py
+++ tests/stockutil/test_ticker.py
@@ -32,3 +32,43 @@ def test_ticker_symbol_above_moving_average(ogn,goev):
     # 低于10周线
     assert goev.symbol_above_moving_average(10) == False
     assert goev.symbol_above_moving_average(100) == False
+
+
+def test_ticker_cal_profit(shared_datadir):
+    """测试定投结果"""
+    aapl = Ticker("AAPL", "local", f"{shared_datadir}", starttime=datetime(2021, 7, 1), endtime=datetime(2021, 8, 20))
+    aapl.load_data()
+    xmm = [
+        [datetime(2021,7,7),144.35],
+        [datetime(2021,7,14),148.93],
+        [datetime(2021,7,21),145.18],
+        [datetime(2021,7,28),144.76],
+        [datetime(2021,8,4),146.73],
+        [datetime(2021,8,11),145.86],
+        [datetime(2021,8,18),146.36]
+        ]
+    dmm = [[datetime(2021,7,14),148.93],[datetime(2021,8,11),145.86]]
+    # xmm 每次买100的所有成本
+    xmm_unit = 0
+    for i in xmm:
+        xmm_unit += 100 / i[1]
+    # dmm 每次买100的所有成本
+    dmm_unit = 0
+    for i in dmm:
+        dmm_unit += 100 / i[1]
+    # 8月20日价格 148.19
+    end_price = aapl.df.loc[datetime(2021,8,20),"Close"]
+    assert end_price == 148.19
+    # 利润率：
+    xmm_profit_rate = ( xmm_unit * end_price - 700) / 700
+    dmm_profit_rate = ( dmm_unit * end_price - 200) / 200
+    aapl.cal_profit()
+    print("xmm",aapl.xmm_profit,"dmm",aapl.dmm_profit)
+    # xmm {'current_profit': 710.4488392607637, 'total_principle': 700, 'profit_percentage': 0.014926913229662553} 
+    # dmm {'current_profit': 201.10054445786588, 'total_principle': 200, 'profit_percentage': 0.005502722289329354}
+    assert xmm_unit * end_price == aapl.xmm_profit["current_profit"]
+    assert dmm_unit * end_price == aapl.dmm_profit["current_profit"]
+    assert aapl.xmm_profit["total_principle"] == 700
+    assert aapl.dmm_profit["total_principle"] == 200
+    assert aapl.xmm_profit["profit_percentage"] == xmm_profit_rate
+    assert aapl.dmm_profit["profit_percentage"] == dmm_profit_rate
--- tests/util/utils_test.py
+++ tests/util/utils_test.py
@@ -15,16 +15,20 @@ def test_get_date_list():
 
 def test_get_target_date():
     from util.utils import get_target_date
-    ds = get_target_date(datetime(2021, 8, 1), datetime(2021, 8, 31))
+    ds = get_target_date(datetime(2021, 7, 1), datetime(2021, 8, 20))
     assert len(ds) == 2
     print(ds)
-    assert len(ds['xmm']) == 4
-    assert ds['xmm'][0] == datetime(2021,8,4)
-    assert ds['xmm'][1] == datetime(2021,8,11)
-    assert ds['xmm'][2] == datetime(2021,8,18)
-    assert ds['xmm'][3] == datetime(2021,8,25)
-    assert len(ds['dmm']) == 1
-    assert ds['dmm'][0] == datetime(2021,8,11)
+    assert len(ds['xmm']) == 7
+    assert ds['xmm'][0] == datetime(2021,7,7)
+    assert ds['xmm'][1] == datetime(2021,7,14)
+    assert ds['xmm'][2] == datetime(2021,7,21)
+    assert ds['xmm'][3] == datetime(2021,7,28)
+    assert ds['xmm'][4] == datetime(2021,8,4)
+    assert ds['xmm'][5] == datetime(2021,8,11)
+    assert ds['xmm'][6] == datetime(2021,8,18)
+    assert len(ds['dmm']) == 2
+    assert ds['dmm'][0] == datetime(2021,7,14)
+    assert ds['dmm'][1] == datetime(2021,8,11)
 
 def test_get_week_num():
     from util.utils import get_week_num
