9fc2f7d8ca02abf7645ed8c296d0f9d317547378
Danny Huang <huangdong@gmail.com>
2021-09-16 09:23:06 -0400

    去除注释，去测试环境啦~

--- sendxyh.py
+++ sendxyh.py
@@ -83,7 +83,6 @@ if __name__ == '__main__':
         for index in ['nasdaq','nyse']:
             i = Index(index,from_s="markets",local_store=config.config_path,endtime=target_date)
             i.compare_market_volume()
-        # i.compare_market_volume()
             notify_message += f"{i.market_volume_msg}\n"
             admin_message += i.err_msg
         
