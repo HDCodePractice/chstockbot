52ab651491f57dc1cde55d0627ef54c62e236eb7
lben1216 <lben1216@hotmail.com>
2021-10-17 00:12:00 -0400

    改进测试用例 测试新的函数

--- tests/cmdproc/test_mmtcmd.py
+++ tests/cmdproc/test_mmtcmd.py
@@ -14,32 +14,34 @@ def test_mmt_all_para(aapl):
 从2021年08月02日定投 #小毛毛 AAPL，到2021年08月20日累计投入 300元，到昨日市值为 303.84 元，利润为 1.28%
 从2021年08月02日定投 #大毛毛 AAPL，到2021年08月20日累计投入 100元，到昨日市值为 101.60 元，利润为 1.60%\n"""
     buttons = [[
-        InlineKeyboardButton(f"20210801",callback_data=f"aapl:20210801:20210820:uid"),
-        InlineKeyboardButton(f"过去一年",callback_data=f"aapl:20200820:20210820:uid"),
-        InlineKeyboardButton(f"过去10年",callback_data=f"aapl:20111019:20210820:uid")
+        InlineKeyboardButton(f"20210801",callback_data=f"aapl:2021-08-01:2021-08-20:uid"),
+        InlineKeyboardButton(f"过去一年",callback_data=f"aapl:2020-08-20:2021-08-20:uid"),
+        InlineKeyboardButton(f"过去10年",callback_data=f"aapl:2011-10-19:2021-08-20:uid")
     ]]
     r,b = mmtcmd.process_income_message(msg,"uid")
+    profit_msg = mmtcmd.process_ticker_profit("aapl","2021-08-01","2021-08-20")
     #assert b == buttons
     assert r == button_reply   
-    #assert rmsg == mmtcmd.profit_list["1"]
+    assert rmsg == profit_msg
 
 
 def test_mmt_3para(aapl):
     # 用户会发出三个参数
     msg = "/mmt aapl 20210101"
     button_reply = f"由于未检测到或只检测到部分日期参数，毛毛投即将使用的日期参数为:2021-01-01/{datetime.today().date()}\n股票代码：aapl\n请选择想要进行毛毛投利润率计算的日期：\n"
-    rmsg = """
-从2021年01月04日定投 #小毛毛 AAPL，到2021年10月08日累计投入 52200元，到昨日市值为 267678.34 元，利润为 412.79%
-从2021年01月04日定投 #大毛毛 AAPL，到2021年10月08日累计投入 13700元，到昨日市值为 70186.60 元，利润为 412.31%\n"""
+    rmsg = """由于起始日期2021-01-01的数据不存在，自动转为最近的有数据的日期:2021-01-04
+从2021年01月04日定投 #小毛毛 AAPL，到2021年08月20日累计投入 3300元，到昨日市值为 3716.19 元，利润为 12.61%
+从2021年01月04日定投 #大毛毛 AAPL，到2021年08月20日累计投入 800元，到昨日市值为 898.78 元，利润为 12.35%\n"""
     buttons = [[
-        InlineKeyboardButton(f"20210801",callback_data=f"aapl:20210101:20210820:uid"),
-        InlineKeyboardButton(f"过去一年",callback_data=f"aapl:20200820:20210820:uid"),
-        InlineKeyboardButton(f"过去10年",callback_data=f"aapl:20111019:20210820:uid")
+        InlineKeyboardButton(f"20210801",callback_data=f"aapl:2021-01-01:2021-08-20:uid"),
+        InlineKeyboardButton(f"过去一年",callback_data=f"aapl:2020-08-20:2021-08-20:uid"),
+        InlineKeyboardButton(f"过去10年",callback_data=f"aapl:2011-10-19:2021-08-20:uid")
     ]]
     r,b = mmtcmd.process_income_message(msg,"uid")
+    profit_msg = mmtcmd.process_ticker_profit("aapl","2021-01-01","2021-08-20")
     #assert b == buttons
     assert r == button_reply   
-    #assert rmsg == mmtcmd.profit_list["1"]
+    assert rmsg == profit_msg
 
 def test_mmt_2para(aapl):
     # 用户会发出两个参数
