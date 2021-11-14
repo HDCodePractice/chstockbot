41354fac5b9651c9ee7fe3d2d827ad718a2a1d3c
lben1216 <lben1216@hotmail.com>
2021-11-14 01:43:27 -0500

    根据 原型设计 重新 改写 代码

--- cmdproc/ytmusiccmd.py
+++ cmdproc/ytmusiccmd.py
@@ -7,7 +7,7 @@ from telegram.inline.inlinekeyboardmarkup import InlineKeyboardMarkup
 import datetime,os,config
 from bot import sendmsg
 from config import ENV
-from util.youtube import download_youtube,YoutubeDLError
+from util.youtube import download_youtube,YoutubeDLError,get_info,init_yt
 from util.tgutil import get_user_link, delay_del_msg, get_group_info
 
 admingroup = ENV.ADMIN_GROUP
@@ -15,23 +15,38 @@ groups = ENV.GROUPS
 admins = ENV.ADMINS
 debug= ENV.DEBUG
 
+pic = "https://c.tenor.com/XasjKGMk_wAAAAAC/load-loading.gif" #需要被转成ENV变量
+
+
 def ytmusic_command(update: Update, context: CallbackContext):
     alert_message = "输入格式不对，请使用 /y url这样的格式查询"
     incoming_message = update.effective_message
+    chat_id = update.effective_chat.id
     user = update.effective_user
     if len(incoming_message.text.split(' ')) <= 1:
         incoming_message.reply_text(alert_message)
         return
     url_link = incoming_message.text.split(' ')[-1]
     try:
-        incoming_message.reply_text(f"亲爱的{user.full_name}，正在为您下载音乐，请稍等")
+        info = get_info(url_link)
+        if info == None:
+            incoming_message.reply_text(f"哥们儿您输入的网址好像不存在啊，请重新输入")
+            return
+        inform_msg=incoming_message.reply_text(f"正在为您下载音乐 大小:{info['filesize']/1024/1024:.2f}MB 预估:{info['waiting_time']:.2f}秒 请耐心等待 点播者：{user.full_name}")
+        download_gif=incoming_message.reply_animation(pic)
         status,output = download_youtube(url_link)
         if status == False:
             reply_msg=f"亲爱的{user.full_name}，bot出错啦，请稍后再试" if output == None else f"亲爱的{user.full_name}，{output}"
             incoming_message.reply_text(reply_msg)
             return
         if status == True:
-            incoming_message.reply_audio(open(os.path.expanduser(output), 'rb'))
+            download_file = f"assets/{info['id']}.{info['ext']}"
+            incoming_message.bot.delete_message(inform_msg.chat_id,inform_msg.message_id)
+            incoming_message.bot.delete_message(download_gif.chat_id,download_gif.message_id)
+            finish_notify_msg=incoming_message.reply_text(f"已从Youtube下载完成 正在上传中 请耐心等待 点播者：{user.full_name}")
+            incoming_message.bot.edit_message_text(chat_id=finish_notify_msg.chat_id,message_id=finish_notify_msg.message_id,text=f"{info['title']}  点播者：{user.full_name}")
+            incoming_message.reply_html(info['thumbnail'])
+            incoming_message.reply_audio(open(download_file, 'rb'))
             
     except Exception as err:
         sendmsg(context.bot,admingroup,f"下载音乐文件报错了，具体信息是{err}",debug)
--- util/youtube.py
+++ util/youtube.py
@@ -3,42 +3,43 @@ import json, asyncio,os
 class YoutubeDLError(Exception):
     pass
 
-
-def download_youtube(url,path="~/Downloads/"):
-    err_msg = ""
-    youtube,url_info_list = get_info(url) #get id/ext information
-    #print(json.dumps(url_info_list))
-    if url_info_list["filesize"] > 10485760 : #判断文件大小
-        err_msg = "您要下载的文件太大了，请重新选择"
-    else:
-        try:
-            #loop = asyncio.get_event_loop()
-            #await loop.run_in_executor(None, youtube.download, [url])
-            dl_file = youtube.download([url]) #download music
-            if dl_file == 0:
-                return True, f"{url_info_list['title']}.{url_info_list['id']}.{url_info_list['ext']}"
-        except Exception as e:
-            err_msg = f"音乐下载报错了，具体错误是{e}"
-    return False, err_msg
-
-def get_info(url):
+def init_yt(ydl_opts=None):
     ydl_opts = {
     "format": "worstaudio[ext=m4a]",
     "geo-bypass": True,
     "nocheckcertificate": True,
-    "outtmpl": "%(title)s.%(id)s.%(ext)s",
+    "outtmpl": "assets/%(id)s.%(ext)s",
     "continuedl" : True
     }
     youtube = YoutubeDL(ydl_opts)
+    return youtube
+
+def download_youtube(url):
+    err_msg = ""
+    youtube = init_yt()
+    #print(json.dumps(url_info_list))
+    try:
+        #loop = asyncio.get_event_loop()
+        #await loop.run_in_executor(None, youtube.download, [url])
+        dl_file = youtube.download([url]) #download music
+        if dl_file == 0:
+            return True, None
+    except Exception as e:
+        err_msg = f"音乐下载报错了，具体错误是{e}"
+    return False, err_msg
+
+def get_info(url):
+    youtube = init_yt()
     try:
         info = youtube.extract_info(url, download=False)
+        info["waiting_time"]= (info["filesize"]/1024/info["tbr"]) if "tbr" in info.keys() else (info["filesize"]/1024/info["abr"])
     except Exception as e:
         return None
     formats = info["formats"]
     thumbnails = info["thumbnails"]
     title = info["title"]
     duration = info["duration"]
-    return youtube,info
+    return info
 
 
 if __name__ == "__main__":
