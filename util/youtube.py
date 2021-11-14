aa6ec98146de074ea365d17baf35d8912cd14d78
lben1216 <lben1216@hotmail.com>
2021-11-13 22:31:19 -0500

    Update youtube.py

--- util/youtube.py
+++ util/youtube.py
@@ -3,17 +3,10 @@ import json, asyncio,os
 class YoutubeDLError(Exception):
     pass
 
+
 def download_youtube(url,path="~/Downloads/"):
-    ydl_opts = {
-        "format": "worstaudio[ext=m4a]",
-        "geo-bypass": True,
-        "nocheckcertificate": True,
-        "outtmpl": path + "%(title)s.%(id)s.%(ext)s",
-        "continuedl" : True
-    }
     err_msg = ""
-    youtube = YoutubeDL(ydl_opts) #init youtube-dl
-    url_info_list = youtube.extract_info(url, download=False) #get id/ext information
+    youtube,url_info_list = get_info(url) #get id/ext information
     #print(json.dumps(url_info_list))
     if url_info_list["filesize"] > 10485760 : #判断文件大小
         err_msg = "您要下载的文件太大了，请重新选择"
@@ -23,13 +16,19 @@ def download_youtube(url,path="~/Downloads/"):
             #await loop.run_in_executor(None, youtube.download, [url])
             dl_file = youtube.download([url]) #download music
             if dl_file == 0:
-                return True, path + f"{url_info_list['title']}.{url_info_list['id']}.{url_info_list['ext']}"
+                return True, f"{url_info_list['title']}.{url_info_list['id']}.{url_info_list['ext']}"
         except Exception as e:
             err_msg = f"音乐下载报错了，具体错误是{e}"
     return False, err_msg
 
 def get_info(url):
-    ydl_opts = {}
+    ydl_opts = {
+    "format": "worstaudio[ext=m4a]",
+    "geo-bypass": True,
+    "nocheckcertificate": True,
+    "outtmpl": "%(title)s.%(id)s.%(ext)s",
+    "continuedl" : True
+    }
     youtube = YoutubeDL(ydl_opts)
     try:
         info = youtube.extract_info(url, download=False)
@@ -39,10 +38,10 @@ def get_info(url):
     thumbnails = info["thumbnails"]
     title = info["title"]
     duration = info["duration"]
-    return info
+    return youtube,info
 
 
 if __name__ == "__main__":
     url = "https://www.youtube.com/watch?v=kYEC7bm7gFs"
-    url = "https://www.youtube.com/watch?v=1PTs1mqrToM"
+    #url = "https://www.youtube.com/watch?v=1PTs1mqrToM"
     print(get_info(url))
