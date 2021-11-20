1127239704e7f18e6e62ba0b78640ba053fe903b
lben1216 <lben1216@hotmail.com>
2021-11-20 15:16:41 -0500

    根据房东哥的建议重新修改了发送信息的部分
    1. 所有下载前的操作都在同一条信息
    2. 下载完成后将文件/缩略图/名称放入一条信息中发出
    3. 删除loading的信息

--- cmdproc/ytmusiccmd.py
+++ cmdproc/ytmusiccmd.py
@@ -32,22 +32,18 @@ def ytmusic_command(update: Update, context: CallbackContext):
         if info == None:
             incoming_message.reply_text(f"哥们儿您输入的网址好像不存在啊，请重新输入")
             return
-        inform_msg=incoming_message.reply_text(f"正在为您下载音乐 大小:{info['filesize']/1024/1024:.2f}MB 预估:{info['waiting_time']:.2f}秒 请耐心等待 点播者：{user.full_name}")
-        download_gif=incoming_message.reply_animation(pic)
+        #inform_msg=incoming_message.reply_text(f"正在为您下载音乐 大小:{info['filesize']/1024/1024:.2f}MB 预估:{info['waiting_time']:.2f}秒 请耐心等待 点播者：{user.full_name}")
+        download_gif=incoming_message.reply_animation(pic,caption=f"正在为您下载音乐 大小:{info['filesize']/1024/1024:.2f}MB 预估:{info['waiting_time']:.2f}秒 请耐心等待 点播者：{user.full_name}")
         status,output = download_youtube(url_link)
         if status == False:
             reply_msg=f"亲爱的{user.full_name}，bot出错啦，请稍后再试" if output == None else f"亲爱的{user.full_name}，{output}"
-            incoming_message.reply_text(reply_msg)
+            incoming_message.edit_caption(reply_msg)
             return
         if status == True:
             download_file = f"assets/{info['id']}.{info['ext']}"
-            incoming_message.bot.delete_message(inform_msg.chat_id,inform_msg.message_id)
-            incoming_message.bot.delete_message(download_gif.chat_id,download_gif.message_id)
-            finish_notify_msg=incoming_message.reply_text(f"已从Youtube下载完成 正在上传中 请耐心等待 点播者：{user.full_name}")
-            incoming_message.bot.edit_message_text(chat_id=finish_notify_msg.chat_id,message_id=finish_notify_msg.message_id,text=f"{info['title']}  点播者：{user.full_name}")
-            incoming_message.reply_html(info['thumbnail'])
-            incoming_message.reply_audio(open(download_file, 'rb'))
-            
+            download_gif.edit_caption(caption=f"已从Youtube下载完成 正在上传中 请耐心等待 点播者：{user.full_name}")
+            incoming_message.reply_audio(open(download_file, 'rb'),thumb=info["thumbnails"][0]["url"].split("?")[0],caption=f"{info['title']}  点播者：{user.full_name}")
+            download_gif.delete()
     except Exception as err:
         sendmsg(context.bot,admingroup,f"下载音乐文件报错了，具体信息是{err}",debug)
         #context.bot.send_message(chat_id=admingroup,text=f"下载音乐文件报错了，具体信息是{err}")
