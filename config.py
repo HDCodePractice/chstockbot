da7ec79ead6f90d6b614aaf3e13d6bec85d78732
老房东 <huangdong@gmail.com>
2022-01-16 21:09:54 -0500

    加入音乐下载功能

--- config.py
+++ config.py
@@ -2,11 +2,12 @@
 
 import json
 import os
-from environs import Env
-from io import StringIO
-from dotenv import load_dotenv
-import requests
 from base64 import b64encode
+from io import StringIO
+
+import requests
+from dotenv import load_dotenv
+from environs import Env
 
 loads = json.loads
 load = json.load
@@ -19,75 +20,84 @@ config_file = ""
 
 CONFIG = {}
 
+
 def load_config():
     global CONFIG
     with open(config_file, 'r') as configfile:
-        CONFIG = load( configfile )
+        CONFIG = load(configfile)
     return CONFIG
+
 
 def save_config():
     file_dir = os.path.split(config_file)[0]
     if not os.path.isdir(file_dir):
         os.makedirs(file_dir)
     with open(config_file, 'w') as configfile:
-        dump(CONFIG, configfile, indent=4,ensure_ascii=False)
+        dump(CONFIG, configfile, indent=4, ensure_ascii=False)
+
 
 def get_json():
-    return dumps(CONFIG,indent=4,ensure_ascii=False)
+    return dumps(CONFIG, indent=4, ensure_ascii=False)
+
 
 def set_default():
-    CONFIG.setdefault("Admin",[])       #管理员id
-    CONFIG.setdefault("Admin_path","")  #Admin Shell Path
+    CONFIG.setdefault("Admin", [])  # 管理员id
+    CONFIG.setdefault("Admin_path", "")  # Admin Shell Path
     save_config()
+
 
 def get_admin_uids():
     if not CONFIG:
         load_config()
     return CONFIG.get("Admin", [])
 
+
 def get_doppler_env(token):
     token_b64 = b64encode(f"{token}:".encode()).decode()
 
     url = "https://api.doppler.com/v3/configs/config/secrets/download"
 
-    querystring = {"format":"env"}
+    querystring = {"format": "env"}
 
     headers = {
         "Accept": "application/json",
         "Authorization": f"Basic {token_b64}"
     }
     try:
-        response = requests.request("GET", url, headers=headers, params=querystring)
+        response = requests.request(
+            "GET", url, headers=headers, params=querystring)
         if response.status_code == 200:
             return response.text
     except Exception:
         pass
     return ""
 
+
 env = Env()
 env.read_env(f"{os.getcwd()}/local.env")
 
 doppler_token = env.str('DOPPLER_TOKEN', default='')
 
-if len(doppler_token) > 0 :
+if len(doppler_token) > 0:
     response = get_doppler_env(doppler_token)
     if len(response) > 0:
         config = StringIO(response)
         load_dotenv(stream=config)
 
+
 class ENV:
-    CONTRIBUTORS="贡献者:老房东、lben1216、stephenzhu01、bernieharvard"
-    WORKDIR=os.getcwd()
+    CONTRIBUTORS = "贡献者:老房东、lben1216、stephenzhu01、bernieharvard"
+    WORKDIR = os.getcwd()
     # BotToken
-    BOT_TOKEN = env.str("BOT_TOKEN", default="") 
+    BOT_TOKEN = env.str("BOT_TOKEN", default="")
     # 是否为DEBUG模式（不发送消息，直接将消息打印到终端）
     DEBUG = env.bool("DEBUG", False)
     # 发送夕阳红的代码和周期
     XYHTICKER = eval(env.str("XYHTICKER", "[]"))
     # 发送目标CHATID
-    XYHCHAT=env.str("XYHCHAT", "")
+    XYHCHAT = env.str("XYHCHAT", "")
     # 发送日志的CHATID
-    XYHLOG=env.str("XYHLOG", "")
+    XYHLOG = env.str("XYHLOG", "")
     # 夕阳红数据源，可以选择 stooq 和 yahoo
     XYHSOURCE = env.list("XYHSOURCE", [])
     # 夕阳红分析的指数列表
@@ -102,6 +112,9 @@ class ENV:
     MMTTICKER = env.list("MMTTICKER", [])
     # 毛毛投发送频道
     MMTCHAT = env.str("MMTCHAT", "")
+    # 音乐ChatIDs
+    MUSIC_GROUP = env.list("MUSIC_GROUP", "")
+
 
 if __name__ == "__main__":
     print(ENV.GROUPS)
---
+++ util/youtube.py
@@ -0,0 +1,67 @@
+from youtube_search import YoutubeSearch
+from yt_dlp import YoutubeDL
+
+
+class YoutubeDLError(Exception):
+    pass
+
+
+def search(keyword):
+    results = YoutubeSearch(keyword, max_results=1).to_dict()
+    url = f"https://youtube.com{results[0]['url_suffix']}"
+    return url
+
+
+def init_yt(ydl_opts=None, download_path="assets"):
+
+    ydl_opts = {
+        "format": "worstaudio[ext=m4a]",
+        "geo-bypass": True,
+        "nocheckcertificate": True,
+        "outtmpl": f"{download_path}/%(title)s.%(id)s.%(ext)s",
+        "continuedl": True
+    }
+    youtube = YoutubeDL(ydl_opts)
+    return youtube
+
+
+def download_youtube(url, path="~/Downloads/"):
+    if path[-1] != "/":
+        path = path + "/"
+    ydl_opts = {
+        "format": "worstaudio[ext=m4a]",
+        "geo-bypass": True,
+        "nocheckcertificate": True,
+        "outtmpl": path + "%(title)s.%(id)s.%(ext)s",
+        "continuedl": True
+    }
+    err_msg = ""
+    youtube = YoutubeDL(ydl_opts)  # init youtube-dl
+    url_info_list = youtube.extract_info(
+        url, download=False)  # get id/ext information
+    # print(json.dumps(url_info_list))
+    if url_info_list["filesize"] > 20971520:  # 判断文件大小
+        err_msg = "您要下载的文件太大了，请重新选择"
+    else:
+        try:
+            dl_file = youtube.download([url])  # download music
+            if dl_file == 0:
+                return True, f"{path}{url_info_list['title'].replace('/', '_')}.{url_info_list['id']}.{url_info_list['ext']}"
+        except Exception as e:
+            err_msg = f"音乐下载报错了，具体错误是{e}"
+    return False, err_msg
+
+
+def get_info(url):
+    youtube = init_yt()
+    try:
+        info = youtube.extract_info(url, download=False)
+        info["waiting_time"] = (info["filesize"]/1024/info["tbr"]
+                                ) if "tbr" in info.keys() else (info["filesize"]/1024/info["abr"])
+    except Exception as e:
+        return None
+    formats = info["formats"]
+    thumbnails = info["thumbnails"]
+    title = info["title"]
+    duration = info["duration"]
+    return info
